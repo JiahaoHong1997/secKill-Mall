bd9358da84a0bb4dea6b343c0e26111bb5a45cf0
JiahaoHong1997 <hongjiahao1997@gmail.com>
2022-02-26 21:14:32 +0800

    atomic

--- validate/getOne.go
+++ validate/getOne.go
@@ -4,25 +4,37 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"sync/atomic"
 )
 
-var sum int64
+var sum int32
 
 // 预存商品数量
-var productNum int64 = 200
+var productNum int32 = 200000
 
 // 互斥锁
 var mutex sync.Mutex
 
+var isSync bool
+
 // 获取秒杀商品
 func GetOneProduct() bool {
-	mutex.Lock()
-	defer mutex.Unlock()
-	if sum < productNum {
-		sum += 1
+
+	if isSync {
+		mutex.Lock()
+		defer mutex.Unlock()
+		if sum < productNum {
+			sum += 1
+			return true
+		}
+		return false
+	} else {
+		atomic.AddInt32(&sum, 1)
+		if sum >= 195000 {
+			isSync = true
+		}
 		return true
 	}
-	return false
 }
 
 func GetProduct(w http.ResponseWriter, r *http.Request) {
